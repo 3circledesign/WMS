f8a47138df0520513b98a80dcaf0c721f7784622
3circledesign <3circledesign@gmail.com>
2026-03-30 11:50:19 +0800

    Add files via upload

---
+++ Start_WMS_Sever.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d C:\Users\akzon\OneDrive\Desktop\WMS\home\atduniaxakzonobel
+echo.
+echo ============================================================
+echo   Starting WMS Warehouse Management System
+echo ============================================================
+echo.
+C:\Users\akzon\AppData\Local\Programs\Python\Python310\python.exe app.py
+pause
